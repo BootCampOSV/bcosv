487efff42a3c5787b3af9d94d6edd842b8a0aa35
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2016-06-06 09:05:18 +0200

    None

--- src/stp2html.py
+++ src/stp2html.py
@@ -1,14 +1,19 @@
 #! /home/uguen/anaconda/bin/python
 from OCC.STEPControl import STEPControl_Reader
-from OCC.Display.WebGl import x3dom_renderer
+from OCC.Display.WebGl import x3dom_renderer_osv
 import pdb
 import sys
+#
+# usage stp2html.py filename level
+#
 filename = sys.argv[1]
-fileout = filename.replace('stp','html')
+level = sys.argv[2]
+pdb.set_trace
+fileout = filename.replace('.stp','')
 step_reader = STEPControl_Reader()
 step_reader.ReadFile(filename)
 step_reader.TransferRoot()
 shape = step_reader.Shape()
-my_renderer = x3dom_renderer.X3DomRenderer(path='.',filename=fileout)
+my_renderer = x3dom_renderer_osv.X3DomRenderer(path='.',filename=fileout,level=level)
 #my_renderer = x3dom_renderer.X3DomRenderer()
 my_renderer.DisplayShape(shape)
