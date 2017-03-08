3b80a0198b6513f8a19c7458ba085e0d12553435
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2017-03-08 16:56:43 +0100

    None

--- src/osvccad.py
+++ src/osvccad.py
@@ -1,8 +1,10 @@
 import ccad.model as cm
 import ccad.display as cd
 import ccad.entities as ce
+from numpy.linalg import svd
 import OCC.Display.SimpleGui as SimpleGui
 import numpy as np 
+import networkx as nx 
 from mayavi.mlab import *
 from OCC.GProp import GProp_GProps as _GProp_GProps
 from OCC.BRepGProp import (brepgprop_VolumeProperties as _brepgprop_VolumeProperties,
@@ -18,10 +20,19 @@ def view(model):
     display, start_display, add_menu, add_function_to_menu = SimpleGui.init_display()
     display.DisplayShape(model.shape, update = True)
     start_display()
+#
+# read a file from the OSV parts
+# The goal is to decomposed this object in a graph of simpler objects 
+#
 
-
+# 1) get a solid from step
 solid  = cm.from_step('level1/ASM0001_ASM_1_ASM.stp')
+# 2) construct entity from solid
 entity = ce.entity(solid)
+
+#
+# An entity is a solid and a graph
+#
 #lshell = t1.subshapes('shell')
 #pc = np.array(t1.center())
 #t2 = t1.copy()
@@ -31,8 +42,23 @@ entity = ce.entity(solid)
 #lvertex = lshell[2].subshapes('vertex')
 #ledge = lshell[2].subshapes('edge')
 #lface = lshell[2].subshapes('face')
+#
+# Loop over all entities 
+#
 for k in entity.G.node:
     data = entity.G.node[k]['entities']
+    pts = np.vstack((data[0,:],data[1,:],data[2,:])).T
+    ptm = np.mean(pts,axis=0)
+    ptsm = pts - ptm 
+    U,S,V = svd(ptsm)
+    S0 = str(int(np.ceil(S[0])))
+    S1 = str(int(np.ceil(S[1])))
+    S2 = str(int(np.ceil(S[2])))
+    sig = S0+'_'+S1+'_'+S2
+    entity.G.node[k]['name']=sig
+    entity.G.node[k]['R']=V
+    entity.G.node[k]['ptm']=ptm
+    # Mayavi vizualisation of point cloud
     points3d(data[0,:],data[1,:],data[2,:],resolution=10,mode='sphere',scale_factor=10)
 #ax.volume(
 s2 = entity.G.node[2]['shape']
